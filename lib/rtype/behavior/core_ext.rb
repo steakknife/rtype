af0a55137168e85c65c907f371b9282a7fc531c4
Sputnik Gugja <sputnikgugja@gmail.com>
2016-04-02 01:38:27 +0900

    add core extension for rtype special behaviors

---
+++ lib/rtype/behavior/core_ext.rb
@@ -0,0 +1,26 @@
+class Object
+	def and(*others)
+		::Rtype::and(self, *others)
+	end
+
+	def nilable
+		::Rtype::nilable(self)
+	end
+	alias_method :or_nil, :nilable
+
+	def not
+		::Rtype::not(self)
+	end
+
+	def and_not(*others)
+		::Rtype::not(self, *others)
+	end
+
+	def or(*others)
+		::Rtype::or(self, *others)
+	end
+
+	def xor(*others)
+		::Rtype::xor(self, *others)
+	end
+end
