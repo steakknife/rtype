03acb46d6e761ab69a44b591c03973ea61f03134
Sputnik Gugja <sputnikgugja@gmail.com>
2016-05-06 01:14:17 +0900

    update version.rb

--- lib/rtype/version.rb
+++ lib/rtype/version.rb
@@ -1,7 +1,7 @@
 module Rtype
 	VERSION = "0.6.0".freeze
 	# rtype java extension version. nil If the extension is not used
-	JAVA_EXT_VERSION = nil
+	JAVA_EXT_VERSION = nil unless defined?(JAVA_EXT_VERSION)
 	# rtype c extension version. nil If the extension is not used
-	NATIVE_EXT_VERSION = nil
+	NATIVE_EXT_VERSION = nil unless defined?(NATIVE_EXT_VERSION)
 end
