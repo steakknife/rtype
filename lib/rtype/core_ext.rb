6aabdb66d1e31f4f40db42d464201b188900e601
Sputnik Gugja <sputnikgugja@gmail.com>
2016-05-05 21:39:56 +0900

    remove Fixnum#ordinalize

--- lib/rtype/core_ext.rb
+++ lib/rtype/core_ext.rb
@@ -82,18 +82,3 @@ class Method
 		::Rtype.type_signatures[owner][name].return_type
 	end
 end
-
-class Fixnum
-	def ordinalize
-	    if (11..13).include?(self % 100)
-			"#{self}th"
-	    else
-			case self % 10
-			when 1; "#{self}st"
-			when 2; "#{self}nd"
-			when 3; "#{self}rd"
-			else "#{self}th"
-			end
-	    end
-	end
-end
