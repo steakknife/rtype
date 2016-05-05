5972abcc9c6bb6288e8f4b536542979a1a725b83
Sputnik Gugja <sputnikgugja@gmail.com>
2016-05-06 00:07:03 +0900

    Add JAVA_EXT_VERSION, NATIVE_EXT_VERSION

--- lib/rtype/version.rb
+++ lib/rtype/version.rb
@@ -1,3 +1,7 @@
 module Rtype
 	VERSION = "0.6.0".freeze
+	# rtype java extension version. nil If the extension is not used
+	JAVA_EXT_VERSION = nil
+	# rtype c extension version. nil If the extension is not used
+	NATIVE_EXT_VERSION = nil
 end
