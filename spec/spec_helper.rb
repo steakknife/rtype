11a57f4d0c3ba0e43795a6f055bdaacb3c9d56ef
Sputnik Gugja <sputnikgugja@gmail.com>
2016-07-21 14:30:27 +0900

    Update spec test, etc.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,11 @@ Coveralls.wear!
 
 require 'rtype'
 require 'rspec'
+
+if !Rtype::NATIVE_EXT_VERSION.nil?
+	puts "Rtype with native extension"
+elsif !Rtype::JAVA_EXT_VERSION.nil?
+	puts "Rtype with java extension"
+else
+	puts "Rtype without native extension"
+end
