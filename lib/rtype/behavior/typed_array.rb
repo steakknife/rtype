080ab740930a3561dcee3dfed00d081b10fbc5f4
Sputnik Gugja <sputnikgugja@gmail.com>
2016-07-21 12:37:47 +0900

    minor change

--- lib/rtype/behavior/typed_array.rb
+++ lib/rtype/behavior/typed_array.rb
@@ -1,15 +1,15 @@
 module Rtype
 	module Behavior
 		class TypedArray < Base
-			def initialize(type_sig)
-				@type_sig = type_sig
-				Rtype.assert_valid_argument_type_sig_element(@type_sig)
+			def initialize(type)
+				@type = type
+				Rtype.assert_valid_argument_type_sig_element(@type)
 			end
 
 			def valid?(value)
 				if value.is_a?(Array)
 					any = value.any? do |e|
-						!Rtype::valid?(@type_sig, e)
+						!Rtype::valid?(@type, e)
 					end
 					!any
 				else
@@ -18,7 +18,7 @@ module Rtype
 			end
 
 			def error_message(value)
-				"Expected #{value.inspect} to be an array with type #{@type_sig.inspect}"
+				"Expected #{value.inspect} to be an array with type #{@type.inspect}"
 			end
 		end
 	end
