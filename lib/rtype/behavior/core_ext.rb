3209b7be566e2353370673a820354ba2f1bab81b
Sputnik Gugja <sputnikgugja@gmail.com>
2016-07-28 04:27:11 +0900

    Update doc comment

--- lib/rtype/behavior/core_ext.rb
+++ lib/rtype/behavior/core_ext.rb
@@ -1,27 +1,33 @@
 class Object
+	# @return [Rtype::Behavior::And]
 	def and(*others)
 		::Rtype::and(self, *others)
 	end
 
+	# @return [Rtype::Behavior::Nilable]
 	def nilable
 		::Rtype::nilable(self)
 	end
 	alias_method :or_nil, :nilable
 
+	# @return [Rtype::Behavior::Not]
 	def not
 		::Rtype::not(self)
 	end
 
+	# @return [Rtype::Behavior::Xor]
 	def xor(*others)
 		::Rtype::xor(self, *others)
 	end
 end
 
 class Array
+	# @return [Rtype::Behavior::TypedArray]
 	def self.of(type_sig)
 		::Rtype::Behavior::TypedArray.new(type_sig)
 	end
 	
+	# @return [Rtype::Behavior::And]
 	def comb
 		::Rtype::Behavior::And[*self]
 	end
@@ -30,30 +36,40 @@ end
 class Num
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Numeric and > 2
+	#   rtype [Num > 2] => Any
 	def self.>(x)
 		lambda { |obj| obj.is_a?(Numeric) && obj > x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Numeric and > 2
+	#   rtype [Num > 2] => Any
 	def self.>=(x)
 		lambda { |obj| obj.is_a?(Numeric) && obj >= x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Numeric and > 2
+	#   rtype [Num > 2] => Any
 	def self.<(x)
 		lambda { |obj| obj.is_a?(Numeric) && obj < x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Numeric and > 2
+	#   rtype [Num > 2] => Any
 	def self.<=(x)
 		lambda { |obj| obj.is_a?(Numeric) && obj <= x }
 	end
 	
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Numeric and > 2
+	#   rtype [Num > 2] => Any
 	def self.==(x)
 		lambda { |obj| obj.is_a?(Numeric) && obj == x }
 	end
@@ -62,30 +78,40 @@ end
 class Int
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Integer and > 2
+	#   rtype [Int > 2] => Any
 	def self.>(x)
 		lambda { |obj| obj.is_a?(Integer) && obj > x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Integer and > 2
+	#   rtype [Int > 2] => Any
 	def self.>=(x)
 		lambda { |obj| obj.is_a?(Integer) && obj >= x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Integer and > 2
+	#   rtype [Int > 2] => Any
 	def self.<(x)
 		lambda { |obj| obj.is_a?(Integer) && obj < x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Integer and > 2
+	#   rtype [Int > 2] => Any
 	def self.<=(x)
 		lambda { |obj| obj.is_a?(Integer) && obj <= x }
 	end
 	
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Integer and > 2
+	#   rtype [Int > 2] => Any
 	def self.==(x)
 		lambda { |obj| obj.is_a?(Integer) && obj == x }
 	end
@@ -94,30 +120,40 @@ end
 class Flo
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Float and > 2
+	#   rtype [Flo > 2] => Any
 	def self.>(x)
 		lambda { |obj| obj.is_a?(Float) && obj > x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Float and > 2
+	#   rtype [Flo > 2] => Any
 	def self.>=(x)
 		lambda { |obj| obj.is_a?(Float) && obj >= x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Float and > 2
+	#   rtype [Flo > 2] => Any
 	def self.<(x)
 		lambda { |obj| obj.is_a?(Float) && obj < x }
 	end
 
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Float and > 2
+	#   rtype [Flo > 2] => Any
 	def self.<=(x)
 		lambda { |obj| obj.is_a?(Float) && obj <= x }
 	end
 	
 	# @param [Numeric] x
 	# @return [Proc]
+	# @example Value must be a Float and > 2
+	#   rtype [Flo > 2] => Any
 	def self.==(x)
 		lambda { |obj| obj.is_a?(Float) && obj == x }
 	end
